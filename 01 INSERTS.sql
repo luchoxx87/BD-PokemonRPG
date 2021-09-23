829cf12ab4e4ef9a8a195e5977eac89c614a1d4f
LucasMedina04 <medina.lucas.e@gmail.com>
2021-09-23 15:12:32 -0300

    Update 01 INSERTS.sql

--- 01 INSERTS.sql
+++ 01 INSERTS.sql
@@ -23,6 +23,15 @@ START TRANSACTION;
 
     INSERT INTO Especie (idEspecie, idTipo, idTipo2, nombre, peso, altura, probabilidadH)
                 VALUES  (1, 12, 17, 'Bulbasaur', 6.9, 0.7, 12.5),
-                        (11, 3, null, 'Metapod', 9.9, 0.7, 50);
+                        (11, 3, null, 'Metapod', 9.9, 0.7, 50),
+                        (12, 3, 18, 'Butterfree', 32, 1.1, 50),
+                        (13, 3, 17, 'Weedle', 3.2, 0.3, 50),
+                        (14, 3, 17, 'Kakuna', 10, 0.6, 50),
+                        (15, 3, 17, 'Beedrill', 29.5, 1, 50),
+                        (16, 11, 18,'Pidgey', 1.8, 0.3, 50),
+                        (17, 11, 18,'Pidgeotto', 30, 1.1, 50),
+                        (18, 11, 18,'Pidgeot', 39.5, 1.5, 50),
+                        (19, 11, null,'Rattata', 3.5, 0.3, 50),
+                        (20, 11, null,'Raticate', 18.5, 0.7, 50);
 
 COMMIT;
