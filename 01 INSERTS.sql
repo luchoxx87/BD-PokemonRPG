af4457ea79f4a4879523e5e06e3b6637cec1e320
luchoxx87 <neoluis87@gmail.com>
2021-09-23 15:42:00 -0300

    Agregadas especies del 2 al 10
    Creado el README

--- 01 INSERTS.sql
+++ 01 INSERTS.sql
@@ -23,6 +23,14 @@ START TRANSACTION;
 
     INSERT INTO Especie (idEspecie, idTipo, idTipo2, nombre, peso, altura, probabilidadH)
                 VALUES  (1, 12, 17, 'Bulbasaur', 6.9, 0.7, 12.5),
-                        (11, 3, null, 'Metapod', 9.9, 0.7, 50);
+                        (2, 12, 17, 'Ivysaur', 13, 1, 12.5),
+                        (3, 12, 17, 'Venusaur', 100, 2, 12.5),
+                        (4, 7, NULL, 'Charmander', 8.5, 0.6, 12.5),
+                        (5, 7, NULL, 'Charmeleon', 19, 1.1, 12.5),
+                        (6, 7, 18, 'Charizard', 90.5, 1.7, 12.5),
+                        (7, 2, NULL, 'Squirtle', 9, 0.5, 12.5),
+                        (8, 2, NULL, 'Wartortle', 22.5, 1, 12.5),
+                        (9, 2, NULL, 'Blastoise', 85.5, 1.6, 12.5),
+                        (11, 3, NULL, 'Metapod', 9.9, 0.7, 50);
 
 COMMIT;
---
+++ README.md
@@ -0,0 +1,4 @@
+<h1 align="center">PokemonRPG</h1>
+
+## DER
+<img src="doc/der.png">
